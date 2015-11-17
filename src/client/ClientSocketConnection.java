771df65b4cbc8e180da324e0b72f98273e074c9b
Max Lindblom <max.g.lindblom@gmail.com>
2015-11-17 17:20:22 +0100

    Added logic to ServerConnection classes

--- src/client/ClientSocketConnection.java
+++ src/client/ClientSocketConnection.java
@@ -1,5 +1,39 @@
 package client;
 
-public class ClientSocketConnection extends Thread{
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientSocketConnection extends Thread {
+	InputStream is;
+	OutputStream os;
+
+	// Creates a socket connection and sends an arbitrary byte array
+	public ClientSocketConnection() throws UnknownHostException, IOException {
+		Socket s = new Socket("argus-7.student.lth.se", 6667);
+		s.setTcpNoDelay(true);
+		is = s.getInputStream();
+		os = s.getOutputStream();
+		byte[] data = new byte[100];
+		os.write(data, 0, 100);
+	}
+
+	// Reads the contents of data, one byte at a time.
+	public void read(byte[] data) throws IOException {
+		int read = 0;
+		while (read < 100) {
+			int n = is.read(data, read, 100 - read); // Blocking
+			if (n == -1)
+				throw new IOException();
+			read += n;
+		}
+	}
+
+	// Writes data to the Output Stream
+	public void write(byte[] data) throws IOException {
+		os.write(data, 0, 100);
+	}
 
 }
--- src/server/ServerSocketConnection.java
+++ src/server/ServerSocketConnection.java
@@ -1,5 +1,48 @@
 package server;
 
-public class ServerSocketConnection extends Thread{
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSocketConnection extends Thread {
+	InputStream is;
+	OutputStream os;
+
+	// Creates a socket connection, accepts, reads the arbitrary byte array and
+	// responds with 1.
+	public ServerSocketConnection() throws IOException {
+		ServerSocket ss = new ServerSocket(6667);
+		Socket s = ss.accept();
+		s.setTcpNoDelay(true);
+		is = s.getInputStream();
+		os = s.getOutputStream();
+		byte[] data = new byte[100];
+		int read = 0;
+		while (read < 100) {
+			int n = is.read(data, read, 100 - read); // Blocking
+			if (n == -1)
+				throw new IOException();
+			read += n;
+		}
+		os.write(1);
+	}
+
+	// Reads the contents of data, one byte at a time.
+	public void read(byte[] data) throws IOException {
+		int read = 0;
+		while (read < 100) {
+			int n = is.read(data, read, 100 - read); // Blocking
+			if (n == -1)
+				throw new IOException();
+			read += n;
+		}
+	}
+
+	// Writes data to the Output Stream
+	public void write(byte[] data) throws IOException {
+		os.write(data, 0, 100);
+	}
 
 }
